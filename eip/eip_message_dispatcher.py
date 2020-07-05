45706d856898094d40c80b2a1c56d0c0936529cd
Joe Ryan <jr@aphyt.com>
2020-07-05 11:53:30 -0700

    More parsing methods for CommonPacketFormat

--- eip/eip_message_dispatcher.py
+++ eip/eip_message_dispatcher.py
@@ -8,21 +8,23 @@ import binascii
 from typing import List
 
 
-class CIPMessage:
+class CIPRequest:
     """
+
+    1756-pm020_-en-p.pdf 17 of 94
+
     * Read Tag Service (0x4c)
     * Read Tag Fragmented Service (0x52)
     * Write Tag Service (0x4d)
     * Write Tag Fragmented Service (0x53)
     * Read Modify Write Tag Service (0x4e)
-    * Get_Instance_Attribute_List Service (Request) (0x55)
+
     """
     READ_TAG_SERVICE = b'\x4c'
     READ_TAG_FRAGMENTED_SERVICE = b'\x52'
     WRITE_TAG_SERVICE = b'\x4d'
     WRITE_TAG_FRAGMENTED_SERVICE = b'\x53'
     READ_MODIFY_WRITE_TAG_SERVICE = b'\x4e'
-    GET_INSTANCE_ATTRIBUTE_LIST = b'\x55'
 
     def __init__(self, request_service: bytes, request_path: bytes, request_data: bytes = b''):
         """
@@ -49,7 +51,23 @@ class CIPMessage:
                self.request_path + self.request_data
 
 
+class CIPReply:
+    def __init__(self, reply_bytes: bytes):
+        self.reply_service = reply_bytes[0:1]
+        self.reserved = reply_bytes[1:2]
+        self.general_status = reply_bytes[2:3]
+        self.extended_status_size = reply_bytes[3:4]
+        # TODO Research any replies that use this. It's usually zero, so I am guessing it is in words
+        extended_status_byte_offset = int.from_bytes(self.extended_status_size, 'little') * 2
+        self.extended_status = reply_bytes[4:extended_status_byte_offset]
+        self.reply_data = reply_bytes[4+extended_status_byte_offset:len(reply_bytes)]
+
+    def value(self):
+        pass
+
+
 class DataAndAddressItem:
+    """EIP-CIP-V2-1.0.pdf Table 2-7.2 – Data and Address item format"""
     NULL_ADDRESS_ITEM = b'\x00\x00'
     CONNECTED_TRANSPORT_PACKET = b'\xb1\x00'
     UNCONNECTED_MESSAGE = b'\xb2\x00'
@@ -58,19 +76,21 @@ class DataAndAddressItem:
     SOCKADDR_INFO_TARGET_TO_ORIGINATOR = b'\x01\x80'
     SEQUENCED_ADDRESS_ITEM = b'\x02\x80'
 
-    def __init__(self, type_id, command: bytes):
+    def __init__(self, type_id, data: bytes):
         self.type_id = type_id
-        self.length = len(command).to_bytes(2, 'little')
-        self.command = command
+        self.length = len(data).to_bytes(2, 'little')
+        self.data = data
 
     def from_bytes(self, bytes_data_address_item: bytes):
-        pass
+        self.type_id = bytes_data_address_item[0:2]
+        self.length = bytes_data_address_item[2:4]
+        self.data = bytes_data_address_item[4:]
 
     def bytes(self):
-        return self.type_id + self.length + self.command
+        return self.type_id + self.length + self.data
 
 
-class CIPCommonPacketFormat:
+class CommonPacketFormat:
     def __init__(self, packets: List[DataAndAddressItem]):
         self.packets = [DataAndAddressItem(DataAndAddressItem.NULL_ADDRESS_ITEM, b''),
                         DataAndAddressItem(DataAndAddressItem.NULL_ADDRESS_ITEM, b'')]
@@ -88,6 +108,15 @@ class CIPCommonPacketFormat:
     def from_bytes(self, bytes_common_packet_format: bytes):
         self.item_count = int.from_bytes(bytes_common_packet_format[0:2], 'little')
         self.packet_bytes = bytes_common_packet_format[2:]
+        packet_offset = 0
+        packet_index = 0
+        while packet_offset < len(self.packet_bytes):
+            data_address_item_id = self.packet_bytes[packet_offset: packet_offset+2]
+            data_address_item_length = int.from_bytes(self.packet_bytes[packet_offset+2: packet_offset+4], 'little')
+            data_address_item_data = self.packet_bytes[packet_offset+4: packet_offset+data_address_item_length]
+            self.packets[packet_index] = DataAndAddressItem(data_address_item_id, data_address_item_data)
+            packet_offset = packet_offset + 4 + data_address_item_length
+            packet_index = packet_index + 1
 
     def bytes(self):
         return self.item_count.to_bytes(2, 'little') + self.packet_bytes
@@ -111,6 +140,14 @@ class CommandSpecificData:
 
 
 class EIPMessage:
+    """
+    EIP Message
+    |-Command Specific Data
+      |-Common Packet Format
+        |-Data And Address Item
+          |-CIP Message
+            |-Route Path
+    """
     def __init__(self, command=b'\x00\x00', command_data=b'', session_handle_id=b'\x00\x00\x00\x00',
                  status=b'\x00\x00\x00\x00', sender_context_data=b'\x00\x00\x00\x00\x00\x00\x00\x00',
                  command_options=b'\x00\x00\x00\x00'):
@@ -124,6 +161,9 @@ class EIPMessage:
 
 
 class EIP:
+    """
+    EIP
+    """
     explicit_message_port = 44818
     null_address_item = b'\x00\x00\x00\x00'
     cip_handle = b'\x00\x00\x00\x00'
@@ -180,11 +220,11 @@ class EIP:
         return received_eip_message
 
     def read_tag(self, tag_name: str):
-        request_path = CIPMessage.tag_request_path(tag_name)
-        cip_message = CIPMessage(CIPMessage.READ_TAG_SERVICE, request_path, b'\x01\x00')
+        request_path = CIPRequest.tag_request_path(tag_name)
+        cip_message = CIPRequest(CIPRequest.READ_TAG_SERVICE, request_path, b'\x01\x00')
         data_address_item = DataAndAddressItem(DataAndAddressItem.UNCONNECTED_MESSAGE, cip_message.bytes())
         packets = [data_address_item]
-        common_packet_format = CIPCommonPacketFormat(packets)
+        common_packet_format = CommonPacketFormat(packets)
         command_specific_data = CommandSpecificData(encapsulated_packet=common_packet_format.bytes())
         return self.send_rr_data(command_specific_data.bytes())
 
@@ -212,11 +252,11 @@ class EIP:
         return response
 
     def get_variable(self, route_path, command_data=b''):
-        cip_message = CIPMessage(b'\x01',
+        cip_message = CIPRequest(b'\x01',
                                  route_path, b'')
         data_address_item = DataAndAddressItem(DataAndAddressItem.UNCONNECTED_MESSAGE, cip_message.bytes())
         packets = [data_address_item]
-        common_packet_format = CIPCommonPacketFormat(packets)
+        common_packet_format = CommonPacketFormat(packets)
         command_specific_data = CommandSpecificData(encapsulated_packet=common_packet_format.bytes())
         return self.send_rr_data(command_specific_data.bytes())
 
