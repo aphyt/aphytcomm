0c222174c8708bd808ccc01a72b6383616d2f591
Joe Ryan <jr@aphyt.com>
2020-12-21 09:24:56 -0800

    Correct license version

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aphyt",
-    version="0.1.1",
+    version="0.1.3",
     author="Joseph Ryan",
     author_email="jr@aphyt.com",
     description="A library to communicate with Omron NX and NJ PLC and motion controllers",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://bitbucket.org/jryan/aphytcomm/src/master/",
-    packages=["aphyt",],
+    packages=setuptools.find_packages(exclude=("tests", "nxmessageservice", "nxmessaging", "examples")),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.5',
