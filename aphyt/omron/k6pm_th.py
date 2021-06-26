aba245eb238ab0885bf9323147833cd7260023df
Joe Ryan <jr@aphyt.com>
2021-06-26 14:30:14 -0700

    add sensor monitor reads

--- aphyt/omron/k6pm_th.py
+++ aphyt/omron/k6pm_th.py
@@ -4,12 +4,11 @@ __maintainer__ = "Joseph Ryan"
 __email__ = "jr@aphyt.com"
 
 from aphyt.eip import *
-
+from decimal import *
 
 class CIPService:
     def __init__(self):
         class_id = None
-
 
 
 class K6PMTHEIP(EIP):
@@ -50,3 +49,48 @@ class K6PMTHEIP(EIP):
         sensor_in_position_adjustment_mode = CIPUnsignedInteger()
         sensor_in_position_adjustment_mode.data = self.get_attribute_single_service(read_address).reply_data
         return sensor_in_position_adjustment_mode
+
+    def sensor_version(self, sensor_number: int):
+        instance_id = struct.pack("<B", sensor_number)
+        read_address = address_request_path_segment(class_id=b'\x75\x03', instance_id=instance_id, attribute_id=b'\x64')
+        sensor_version = CIPUnsignedInteger()
+        sensor_version.data = self.get_attribute_single_service(read_address).reply_data
+        return sensor_version
+
+    def sensor_status(self, sensor_number: int):
+        instance_id = struct.pack("<B", sensor_number)
+        read_address = address_request_path_segment(class_id=b'\x75\x03', instance_id=instance_id, attribute_id=b'\x65')
+        sensor_status = CIPUnsignedInteger()
+        sensor_status.data = self.get_attribute_single_service(read_address).reply_data
+        return sensor_status
+
+    def sensor_alarm_status(self, sensor_number: int):
+        instance_id = struct.pack("<B", sensor_number)
+        read_address = address_request_path_segment(class_id=b'\x75\x03', instance_id=instance_id, attribute_id=b'\x66')
+        sensor_alarm_status = CIPUnsignedInteger()
+        sensor_alarm_status.data = self.get_attribute_single_service(read_address).reply_data
+        return sensor_alarm_status
+
+    def internal_temperature(self, sensor_number: int):
+        instance_id = struct.pack("<B", sensor_number)
+        read_address = address_request_path_segment(class_id=b'\x75\x03', instance_id=instance_id, attribute_id=b'\x67')
+        internal_temperature = CIPUnsignedInteger()
+        internal_temperature.data = self.get_attribute_single_service(read_address).reply_data
+        internal_temperature = Decimal(internal_temperature.value())/Decimal(10.0)
+        return internal_temperature
+
+    def internal_maximum_temperature(self, sensor_number: int):
+        instance_id = struct.pack("<B", sensor_number)
+        read_address = address_request_path_segment(class_id=b'\x75\x03', instance_id=instance_id, attribute_id=b'\x68')
+        internal_maximum_temperature = CIPUnsignedInteger()
+        internal_maximum_temperature.data = self.get_attribute_single_service(read_address).reply_data
+        internal_maximum_temperature = Decimal(internal_maximum_temperature.value()) / Decimal(10.0)
+        return internal_maximum_temperature
+
+    def internal_predicted_arrival(self, sensor_number: int):
+        instance_id = struct.pack("<B", sensor_number)
+        read_address = address_request_path_segment(class_id=b'\x75\x03', instance_id=instance_id, attribute_id=b'\x69')
+        internal_predicted_arrival = CIPUnsignedInteger()
+        internal_predicted_arrival.data = self.get_attribute_single_service(read_address).reply_data
+        internal_predicted_arrival = Decimal(internal_predicted_arrival.value()) / Decimal(10.0)
+        return internal_predicted_arrival
