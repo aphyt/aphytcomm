52cc944c8e4e1dc2ffbaf0d4329ac4c2657286fd
Joe Ryan <jr@aphyt.com>
2021-10-30 15:55:27 -0700

    Modify tests to support new online test project

--- aphyt/omron/omron_cip_objects.py
+++ aphyt/omron/omron_cip_objects.py
@@ -7,6 +7,12 @@ from aphyt.cip import *
 
 
 class TCPIPInterfaceObject:
+    """
+    class id, instance id, class attribute id, instance attribute id
+    writable?
+    data type and default value
+    Should CIP objects have a dependency injection that allows easy reading and writing of attributes?
+    """
     class_id = b'\xf5'
 
     def __init__(self):
