58ce614d2d0d7cbecd32abc402ddb94041a41eca
Joe Ryan <jr@aphyt.com>
2022-07-19 14:24:03 -0700

    Add verified write

--- aphyt/tkmi/widgets.py
+++ aphyt/tkmi/widgets.py
@@ -6,3 +6,20 @@ __email__ = "jr@aphyt.com"
 import os
 import tkinter
 from tkinter import ttk
+from aphyt.omron import n_series
+
+
+class PushButton(tkinter.ttk.Button):
+    def __init__(self, master, controller: n_series.NSeries, variable_name: str, **kwargs):
+        super().__init__(master, **kwargs)
+        self.controller = controller
+        self.variable_name = variable_name
+        self.bind('<ButtonPress-1>', self._set_value)
+        self.bind('<ButtonRelease-1>', self._reset_value)
+
+    def _set_value(self, event):
+        self.controller.verified_write_variable(self.variable_name, True)
+
+    def _reset_value(self, event):
+        self.controller.verified_write_variable(self.variable_name, False)
+
