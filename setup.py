eacdb5dbcf0f2ec0c20b16a24cabdc5d0465c43a
Joe Ryan <jr@aphyt.com>
2023-03-07 09:45:15 -0800

    Version setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aphyt",
-    version="0.1.8",
+    version="0.1.9",
     author="Joseph Ryan",
     author_email="jr@aphyt.com",
     description="A library to communicate with Omron NX and NJ PLC and motion controllers",
