ec70abcb2f991283ee49bbe82b4a0d4bdc05cdf7
Joe Ryan <jr@aphyt.com>
2024-03-22 21:09:27 -0700

    Moved package to src folder

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aphyt",
-    version="0.1.18",
+    version="0.1.19",
     author="Joseph Ryan",
     author_email="jr@aphyt.com",
     description="A library to communicate with Omron NX and NJ PLC and motion controllers",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/aphyt/aphytcomm",
-    packages=setuptools.find_packages(exclude=("tests", "examples")),
-    package_data={"aphyt.tkmi": ["transparent_icon.ico", "Transparent_Pixel.png"]},
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    package_data={"aphyt.tkmi": ["Transparent_Pixel.png"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
